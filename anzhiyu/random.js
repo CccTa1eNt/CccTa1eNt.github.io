6af3f7e4af71a21676b450408645f625990fa24f
CccTa1eNt <3757132273@qq.com>
2026-08-17 16:37:38 +0800

    Site updated: 2026-08-17 16:37:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/16/“我的第一篇技术文章”/","2026/08/16/hello-world/"];function toRandomPost(){
+var posts=["2026/08/16/hello-world/","2026/08/16/“我的第一篇技术文章”/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
